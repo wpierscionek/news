b1e89813ad37aa326811447067d6b0ce748fa7dc
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-06-01 17:39:38 -0400

    6/1/2016

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -2,23 +2,36 @@ var path = require('path');
 var cheerio = require('cheerio');
 var request = require('request');
 
+var Note = require('../models/Note.js');
+var Article = require('../models/Article.js');
+
 
 module.exports = function(app) {
 
-    app.get("/", function(req, res) {
-        request('https://news.ycombinator.com/', function(error, response, html) {
+    app.get('/scrape', function(req, res) {
+        request('http://www.echojs.com/', function(error, response, html) {
             var $ = cheerio.load(html);
-            //console.log(response);
-            $('.title').each(function(i, element) {
+            $('article h2').each(function(i, element) {
 
-                var title = $(this).children('a').text();
-                var link = $(this).children('a').attr('href');
-                // console.log(title);
-                // console.log(link);
+                var result = {};
+
+                result.title = $(this).children('a').text();
+                result.link = $(this).children('a').attr('href');
+
+                var entry = new Article(result);
+
+                entry.save(function(err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(doc);
+                    }
+                });
+
 
             });
         });
-          res.send("Scrape Complete");
+        res.send("Scrape Complete");
+    });
 
-});
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var path = require('path');
 
 // App
@@ -9,16 +9,16 @@ var PORT = process.env.PORT || 80;
 
 
 // Database
-// mongoose.connect('mongod://');
-// var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/mongonews');
+var db = mongoose.connection;
 
-// db.on('error', function(err){
-// 	console.log('mongoose:', err);
-// });
+db.on('error', function(err){
+	console.log('mongoose:', err);
+});
 
-// db.once('open', function(err){
-// 	console.log('mongoose connected');
-// });
+db.once('open', function(err){
+	console.log('mongoose connected');
+});
 
 // BodyParser
 app.use(bodyParser.json());
